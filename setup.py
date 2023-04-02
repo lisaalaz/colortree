9d5f2976cb455dc6796c669c79c44e6676a51cee
lisaalaz <lisa.alazraki@zoho.eu>
2023-04-02 16:02:31 +0100

    fixed path

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.md", "r") as f:
 setup(
     name="colortree",
     version="0.0.1",
-    author="LisaAlaz",
+    author="lisaalaz",
     author_email="",
-    description="A package to build, visualize and evaluate decision trees.",
+    description="A package to build, visualize and evaluate colorful decision trees.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/LisaAlaz/ColorTree",
+    url="https://github.com/lisaalaz/colortree",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
